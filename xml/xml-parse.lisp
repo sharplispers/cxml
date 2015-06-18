7fc6b3d72a88a4718045a3b7cb3a6f2a887b9c7e
Paul M. Rodriguez <pmr@ruricolist.com>
2015-06-18 00:13:14 -0500

    Stop reversing the attribute list

--- xml/xml-parse.lisp
+++ xml/xml-parse.lisp
@@ -3850,15 +3850,10 @@
     (sax:end-prefix-mapping (handler *ctx*) (car ns-decl))))
 
 (defun build-attribute-list (attr-alist)
-  ;; fixme: if there is a reason this function reverses attribute order,
-  ;; it should be documented.
-  (let (attributes)
-    (dolist (pair attr-alist)
-      (push (sax:make-attribute :qname (car pair)
-				:value (cdr pair)
-				:specified-p t)
-	    attributes))
-    attributes))
+  (loop for (qname . value) in attr-alist
+        collect (sax:make-attribute :qname qname
+                                    :value value
+                                    :specified-p t)))
 
 (defun check-attribute-uniqueness (attributes)
   ;; 5.3 Uniqueness of Attributes
