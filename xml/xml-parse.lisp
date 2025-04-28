672e1f93126ef466cfc52396e7e45f12c225327b
Alexey Veretennikov <fourier@protonmail.ch>
2025-04-29 00:35:55 +0200

    Issue #11: Fix incorrect type declaration in rod-hash-get
    Add possibility for the variable q to be nil.

--- xml/xml-parse.lisp
+++ xml/xml-parse.lisp
@@ -370,7 +370,7 @@
                  (rod-hashtable-size hashtable))))
     (dolist (q (svref (rod-hashtable-table hashtable) j)
                (values nil nil nil))
-      (declare (type cons q))
+      (declare (type list q))
       (when (rod=** (car q) rod 0 (length (the (simple-array rune (*)) (car q))) start end)
         (return (values (cdr q) t (car q)))))))
 
